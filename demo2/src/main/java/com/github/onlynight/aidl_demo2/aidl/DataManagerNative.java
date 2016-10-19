260ae1d7aae5ddf8601b37ba11e1f13e05344ab6
Wyndam <only.night@qq.com>
2016-10-19 16:08:53 +0800

    添加一些注释。

--- demo2/src/main/java/com/github/onlynight/aidl_demo2/aidl/DataManagerNative.java
+++ demo2/src/main/java/com/github/onlynight/aidl_demo2/aidl/DataManagerNative.java
@@ -13,11 +13,12 @@ import java.util.List;
 /**
  * Created by lion on 2016/10/12.
  */
-
 public abstract class DataManagerNative extends Binder implements IDataManager2 {
 
+    // Binder描述符，唯一标识符
     private static final String DESCRIPTOR = "com.github.onlynight.aidl_demo2.aidl.IDataManager2";
 
+    // 每个方法对应的ID
     private static final int TRANSACTION_getDataCount = IBinder.FIRST_CALL_TRANSACTION;
     private static final int TRANSACTION_getData = IBinder.FIRST_CALL_TRANSACTION + 1;
 
@@ -25,15 +26,23 @@ public abstract class DataManagerNative extends Binder implements IDataManager2 
         attachInterface(this, DESCRIPTOR);
     }
 
+    /**
+     * 将Binder转化为IInterface接口
+     *
+     * @param binder
+     * @return
+     */
     public static IDataManager2 asInterface(IBinder binder) {
         if (binder == null) {
             return null;
         }
+        //同一进程内直接返回
         IInterface iin = binder.queryLocalInterface(DESCRIPTOR);
         if ((iin != null) && (iin instanceof IDataManager2)) {
             return (IDataManager2) iin;
         }
 
+        //不在同一进程使用代理获取远程服务
         return new Proxy(binder);
     }
 
@@ -42,6 +51,18 @@ public abstract class DataManagerNative extends Binder implements IDataManager2 
         return this;
     }
 
+    /**
+     * 我们查看Binder的源码就可以看出实际上transact方法真正的执行体
+     * 是这个onTransact方法。
+     *
+     * @param code  服务器回掉的方法ID，每一个方法都有一个唯一id，
+     *              这样方法回调时可通过id判断回调的方法。
+     * @param data  输入的参数，传递给服务端的参数
+     * @param reply 输出的参数，服务器返回的数据
+     * @param flags 默认传入0
+     * @return
+     * @throws RemoteException 远端服务器无响应抛出该错误。
+     */
     @Override
     protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
         switch (code) {
@@ -63,7 +84,10 @@ public abstract class DataManagerNative extends Binder implements IDataManager2 
         return super.onTransact(code, data, reply, flags);
     }
 
-    static class Proxy implements IDataManager2 {
+    /**
+     * 代理类，调用transact方法。
+     */
+    private static class Proxy implements IDataManager2 {
 
         private IBinder remote;
 
@@ -77,7 +101,10 @@ public abstract class DataManagerNative extends Binder implements IDataManager2 
 
         @Override
         public int getDataCount() throws RemoteException {
+            // 输入参数
             Parcel _data = Parcel.obtain();
+
+            //输出参数
             Parcel _reply = Parcel.obtain();
             int _result;
             try {
