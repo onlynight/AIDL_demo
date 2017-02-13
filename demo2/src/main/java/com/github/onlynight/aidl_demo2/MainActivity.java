d3d749d0253a2de58190a194ce3a583974f18ae9
Wyndam <only.night@qq.com>
2017-02-13 20:13:26 +0800

    修改调用方式

--- demo2/src/main/java/com/github/onlynight/aidl_demo2/MainActivity.java
+++ demo2/src/main/java/com/github/onlynight/aidl_demo2/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -40,17 +42,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void callService(View view) {
-        try {
-            System.out.println(dataManagerService.getDataCount());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(dataManagerService.getDataCount());
 
-            StringBuilder sb = new StringBuilder();
-            for (Data2 data : dataManagerService.getData()) {
-                System.out.println(data.toString());
-                sb.append(data.toString()).append("\n");
+                    StringBuilder sb = new StringBuilder();
+                    for (Data2 data : dataManagerService.getData()) {
+                        System.out.println(data.toString());
+                        sb.append(data.toString()).append("\n");
+                    }
+
+                    Message msg = new Message();
+                    msg.what = 1;
+                    msg.obj = sb.toString();
+                    handler.sendMessage(msg);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
-            textData.setText(sb.toString());
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+        }).start();
     }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == 1) {
+                String content = (String) msg.obj;
+                textData.setText(content);
+            }
+            return false;
+        }
+    });
 }
